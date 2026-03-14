d78465d0f83c646fc3ff6f5d3789688a24c30458
YunaSim <syna02178@gmail.com>
2026-03-15 04:38:00 +0900

    test: cover query rewriting runtime flow

--- rag/__init__.py
+++ rag/__init__.py
@@ -1,5 +1,5 @@
 """RAG Q&A pipeline for CatchUp."""
 
-from rag.qa_chain import QAResult, SourceBlock, delete_document_index, has_document_vectors, index_document, query, retrieve_context
+from rag.qa_chain import QAResult, SourceBlock, delete_document_index, has_document_vectors, index_document, query, retrieve_context, rewrite_query
 
-__all__ = ["index_document", "delete_document_index", "has_document_vectors", "query", "retrieve_context", "QAResult", "SourceBlock"]
+__all__ = ["index_document", "delete_document_index", "has_document_vectors", "query", "retrieve_context", "rewrite_query", "QAResult", "SourceBlock"]
