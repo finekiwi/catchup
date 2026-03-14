99f7811b309c15e7bef88fc95cddcc3a7fc82a40
YunaSim <syna02178@gmail.com>
2026-03-15 01:11:15 +0900

    fix: export delete_document_index from rag __init__.py

--- rag/__init__.py
+++ rag/__init__.py
@@ -1,5 +1,5 @@
 """RAG Q&A pipeline for CatchUp."""
 
-from rag.qa_chain import QAResult, SourceBlock, index_document, query
+from rag.qa_chain import QAResult, SourceBlock, delete_document_index, index_document, query
 
-__all__ = ["index_document", "query", "QAResult", "SourceBlock"]
+__all__ = ["index_document", "delete_document_index", "query", "QAResult", "SourceBlock"]
